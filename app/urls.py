d2b7e9e5f3bba147bfa89e7a2603d4b961d4b314
luisthatguyIZI <71874414+luisthatguyIZI@users.noreply.github.com>
2022-11-07 22:01:09 +0000

    Delete app directory

--- app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns= [
-    path('',views.index,name='index'),
-    path('charts/',views.charts,name='charts'),
-    path('login/',views.login,name="login"),
-    path('tables/',views.tables,name="tables")
-]
